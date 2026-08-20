547afc1b907c1ef35a91458a2c78044e01a6b949
ciomek <ciomek@tutanota.com>
2026-08-20 20:55:03 +0200

    Offline inv

--- src/main/java/ciomek/loon/mqtt/request/InventoryRequest.java
+++ src/main/java/ciomek/loon/mqtt/request/InventoryRequest.java
@@ -1,9 +1,11 @@
 package ciomek.loon.mqtt.request;
 
-import ciomek.loon.mqtt.MQTTClient;
 import ciomek.loon.mqtt.payload.data.ItemStackData;
 import ciomek.loon.mqtt.payload.data.PlayerIdentity;
 import ciomek.loon.mqtt.payload.player.inventory.PlayerInventoryPayload;
+import com.mojang.nbt.tags.CompoundTag;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.player.inventory.container.ContainerInventory;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.entity.player.PlayerServer;
 
@@ -18,18 +20,30 @@ public record InventoryRequest(UUID playerUuid) implements IRequest {
 			.findFirst()
 			.orElse(null);
 
-		if (player == null) {
+		if (player != null) {
+			PlayerInventoryPayload payload =  new PlayerInventoryPayload(
+				new PlayerIdentity(player.uuid, player.username),
+				Arrays.stream(player.inventory.mainInventory).map(ItemStackData::from).toList(),
+				Arrays.stream(player.inventory.armorInventory).map(ItemStackData::from).toList()
+			);
+
+			payload.send();
 			return;
 		}
 
-		PlayerInventoryPayload payload = new PlayerInventoryPayload(
-			new PlayerIdentity(player.uuid, player.username),
-			Arrays.stream(player.inventory.mainInventory)
-				.map(ItemStackData::from)
-				.toList(),
-			Arrays.stream(player.inventory.armorInventory)
-				.map(ItemStackData::from)
-				.toList()
+		CompoundTag data = server.getDimensionWorld(0).getLevelStorage().getPlayerData(null, playerUuid);
+		if (data == null) {
+			return;
+		}
+
+		ContainerInventory inventory = new ContainerInventory(null);
+		inventory.load(data.getList("Inventory"));
+
+		String username = data.containsKey("Nickname") ? data.getString("Nickname") : "";
+		PlayerInventoryPayload payload =  new PlayerInventoryPayload(
+			new PlayerIdentity(playerUuid, username),
+			Arrays.stream(inventory.mainInventory).map(ItemStackData::from).toList(),
+			Arrays.stream(inventory.armorInventory).map(ItemStackData::from).toList()
 		);
 
 		payload.send();
